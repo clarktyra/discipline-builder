348e61e4ae268b0bafd615e8cfdd66813168a299
Clark Tyra <clark.tyra@gmail.com>
2020-04-23 11:53:05 -0700

    changed my server secret

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // Requiring necessary npm packages
+require('dotenv').config();
 var express = require("express");
 var session = require("express-session");
 // Requiring passport as we've configured it
@@ -15,7 +16,7 @@ app.use(express.json());
 app.use(express.static("public"));
 // We need to use sessions to keep track of our user's login status
 app.use(
-  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+  session({ secret: process.env.SERVER_SECRET, resave: true, saveUninitialized: true })
 );
 app.use(passport.initialize());
 app.use(passport.session());
