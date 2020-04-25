ee7afae7ee3047fe888d66509c7fb47fd3dfc930
Clark Tyra <clark.tyra@gmail.com>
2020-04-24 21:55:08 -0700

    created a get rout for grabing the users choices data

--- public/js/members.js
+++ public/js/members.js
@@ -5,9 +5,15 @@ $(document).ready(function() {
 
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
+  // $.get("/api/user_data").then(function(data) {
+  //   $(".member-name").text(data.email);
+  //   // also grab all their posts data
+  //   console.log(data)
+  // });
+
   $.get("/api/user_data").then(function(data) {
-    $(".member-name").text(data.email);
     // also grab all their posts data
+    console.log(data);
   });
 
   function handleSubmit() {
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -57,17 +57,27 @@ module.exports = function(app) {
   });
 
   // Route for getting some data about our user to be used client side
+  // app.get("/api/user_data", function(req, res) {
+  //   if (!req.user) {
+  //     // The user is not logged in, send back an empty object
+  //     res.json({});
+  //   } else {
+  //     // Otherwise send back the user's email and id
+  //     // Sending back a password, even a hashed password, isn't a good idea
+  //     res.json({
+  //       email: req.user.email,
+  //       id: req.user.id
+  //     });
+  //   }
+  // });
+
   app.get("/api/user_data", function(req, res) {
-    if (!req.user) {
-      // The user is not logged in, send back an empty object
-      res.json({});
-    } else {
-      // Otherwise send back the user's email and id
-      // Sending back a password, even a hashed password, isn't a good idea
-      res.json({
-        email: req.user.email,
-        id: req.user.id
-      });
-    }
+    db.Choices.findAll({
+      where: {
+        UserId: req.user.id
+      }
+    }).then(function(choicesData) {
+      res.json(choicesData);
+    });
   });
 };
